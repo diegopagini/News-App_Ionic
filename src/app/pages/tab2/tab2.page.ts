7f36a2be4633e4e742ea73d359698742b406936a
Diego Pagini <diegopaginidev@gmail.com>
2021-09-30 14:36:37 +0200

    headers

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  @ViewChild(IonSegment, { static: true }) segment: IonSegment;
 
-  constructor() {}
+  public categories: string[] = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology',
+  ];
 
+  ngOnInit(): void {
+    this.segment.value = this.categories[0];
+  }
 }
